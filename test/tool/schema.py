f49f921d64a97aa5c1260e65db1fc74297c1c1e8
Ke Li <kel@splunk.com>
2016-11-17 10:26:04 +0800

    ADDON-12197
    Create schema as JSON format with jsl and validate it with jsonschema

---
+++ test/tool/schema.py
@@ -0,0 +1,136 @@
+import jsl
+
+_HTTP_METHODS = ['GET', 'POST', 'PUT', 'DELETE', 'PATCH']
+
+_DATA_FORMAT = ['splunk_xml', 'raw']
+
+_STOP_CONDITIONS = ['empty', 'once']
+_DEFAULT_STOP_CONDITION = 'empty'
+
+_AUTH_TYPES = ['digest', 'basic64']
+
+
+class _Output(jsl.Document):
+    """
+    Represents the common schema definition for outputting configuration.
+    """
+
+    class Options(object):
+        additional_properties = True
+
+    data = jsl.DictField(required=True,
+                         properties={
+                             'format': jsl.StringField(enum=_DATA_FORMAT,
+                                                       required=True),
+                         },
+                         additional_properties=True)
+
+
+class _FileOutput(_Output):
+    """
+    Represents scheme for file outputting configuration which type must
+     be `file` and user can specify file destination, etc in options.
+    """
+    type = jsl.StringField(enum=['file'])
+    options = jsl.DictField(required=True)
+
+
+class _StdoutOutput(_Output):
+    """
+    Represents scheme for stdout outputting configuration.
+    """
+    type = jsl.StringField(enum=['stdout'], required=True)
+    options = jsl.DictField(required=True,
+                            properties={
+                                'host': jsl.StringField(required=True),
+                                'index': jsl.StringField(required=True),
+                                'source': jsl.StringField(required=True),
+                                'sourcetype': jsl.StringField(required=True),
+                            },
+                            additionalProperties=True)
+
+
+class _Checkpoint(jsl.Document):
+    """
+    Represents configuration scheme for saving checkpoint.
+    """
+
+    class Options(object):
+        additional_properties = True
+
+    save = jsl.OneOfField(fields=[jsl.StringField(), jsl.BooleanField()],
+                          required=False,
+                          default=True)
+
+    namespace = jsl.ArrayField(items=[jsl.StringField()],
+                               required=False)
+    content = jsl.DictField(required=True)
+
+
+class _Request(jsl.Document):
+    """
+    Represents config scheme for single request.
+    """
+
+    class Options(object):
+        additional_properties = True
+
+    context = jsl.DictField(required=True)
+
+    auth = jsl.DictField(required=False,
+                         properties={
+                             'type': jsl.StringField(enum=_AUTH_TYPES),
+                             'options': jsl.DictField(),
+                         },
+                         additional_properties=True)
+
+    headers = jsl.DictField(required=True)
+    url = jsl.UriField(required=True)
+    method = jsl.StringField(enum=_HTTP_METHODS, required=True)
+
+    stop_condition = jsl.StringField(enum=_STOP_CONDITIONS,
+                                     required=True,
+                                     default=_DEFAULT_STOP_CONDITION)
+    output = jsl.ArrayField(
+        items=jsl.OneOfField([jsl.DocumentField(_StdoutOutput, as_ref=True),
+                              jsl.DocumentField(_FileOutput, as_ref=True)]),
+        min_items=1
+    )
+    params = jsl.ArrayField(items=[jsl.DictField()], required=False)
+    checkpoint = jsl.DocumentField(_Checkpoint, as_ref=True)
+
+
+class _Proxy(jsl.Document):
+    """
+    Represents proxy configuration scheme. enabled, host and port are required.
+    """
+
+    class Options(object):
+        additional_properties = True
+
+    enabled = jsl.OneOfField([jsl.BooleanField(), jsl.StringField()],
+                             required=True)
+
+    host = jsl.StringField(required=True)
+    port = jsl.OneOfField([jsl.IntField(), jsl.StringField()],
+                          required=True)
+    username = jsl.StringField(required=False)
+    password = jsl.StringField(required=False)
+    rdns = jsl.StringField(required=False)
+    type = jsl.StringField(required=False)
+
+
+class Schema(jsl.Document):
+    """
+    Represents scheme for validating user configuration as JSON format.
+    """
+    __version__ = jsl.StringField(required=True, pattern='[\d\.]+')
+    parameters = jsl.ArrayField(required=True, items=[jsl.StringField()])
+
+    proxy = jsl.DocumentField(_Proxy, required=False, as_ref=True)
+
+    hec = jsl.DictField(required=True)
+    logging = jsl.DictField(required=True)
+    requests = jsl.ArrayField(items=[jsl.DocumentField(_Request, as_ref=True)],
+                              min_items=1,
+                              required=True)
---
+++ test/unit/test_schema.py
@@ -0,0 +1,60 @@
+import json
+import os.path as op
+
+import pytest
+
+from jsonschema import ValidationError
+from jsonschema import validate
+
+
+def _load_json(json_file):
+    current_dir = op.dirname(op.abspath(__file__))
+    with open(op.join(current_dir, 'data', json_file), 'r') as f:
+        return json.loads(f.read())
+
+
+def _load_schema_from_file():
+    schema_file = op.join(op.dirname(op.dirname(op.dirname(op.abspath(__file__)))),
+                          'package', 'bin', 'schema', 'schema.json')
+    with open(schema_file, 'r') as f:
+        return json.loads(f.read())
+
+
+def test_validate_generated_schema():
+    conf = _load_json('data.json')
+    assert conf['__version__'] == '1.0.0'
+    validate(conf, _load_schema_from_file())
+
+
+def test_validate_schema_expect_raise():
+    conf = {}
+    schema = _load_schema_from_file()
+
+    with pytest.raises(ValidationError):
+        validate(conf, schema)
+
+    conf['parameters'] = []
+    conf['__version__'] = '1.0.0'
+    conf['hec'] = {}
+    conf['logging'] = {}
+    conf['requests'] = [
+        {
+            'context': {},
+            'url': 'http://test.com',
+            'checkpoint': {
+                'save': True,
+                'content': {},
+            },
+            'headers': {},
+            'method': 'GET',
+            'stop_condition': 'empty',
+            'output': [{
+                'type': 'file',
+                'data': {
+                    'format': 'raw'
+                },
+                'options': {}
+            }]
+        }
+    ]
+    validate(conf, schema)

c8d743054ec04fccfd8a32e980411a99f4804dce
Jason Xu <xxu@splunk.com>
2016-11-29 19:35:40 +0800

    change directory

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def install_3rdlibs():
     os.chdir(basedir)
     pip_version = os.popen("pip -V").read().rstrip().split()[1]
 
-    target = os.path.join(basedir, "package", "bin", "app_cloud_connect")
+    target = os.path.join(basedir, "package")
 
     install_cmd = "pip install --requirement requirements.txt -i http://repo.splunk.com/artifactory/api/pypi/pypi-virtual/simple --no-compile --no-binary :all: --target " + target
 

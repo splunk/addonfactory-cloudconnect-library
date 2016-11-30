d02011b70027e0080e43668b5ed3ab0dd14ec90f
Jason Xu <xxu@splunk.com>
2016-11-30 14:57:46 +0800

    global settings

---
+++ package/cloudconnectlib/core/util.py
@@ -0,0 +1,12 @@
+def is_true(val):
+    value = str(val).strip().upper()
+    if value in ("1", "TRUE", "T", "Y", "YES"):
+        return True
+    return False
+
+
+def is_false(val):
+    value = str(val).strip().upper()
+    if value in ("0", "FALSE", "F", "N", "NO", "NONE", ""):
+        return True
+    return False

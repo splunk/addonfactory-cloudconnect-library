854d0599f5f5fdeb19fb2f1bbf56e96fd8831f61
Ke Li <kel@splunk.com>
2016-12-01 11:32:33 +0800

    add authenciation

--- package/cloudconnectlib/core/model/__init__.py
+++ package/cloudconnectlib/core/model/__init__.py
@@ -3,5 +3,5 @@ from .global_settings import Proxy, Logging, GlobalSetting
 from .meta import Meta
 from .request import (
     Request, AfterRequest, BeforeRequest, SkipAfterRequest, Checkpoint,
-    Options, Header, Condition, Checkpoint
+    Options, Header, Condition, Checkpoint, BasicAuthorization
 )

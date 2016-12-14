1b0ceeacecb478de5dc4c396e11be7c97db08806
Jason Xu <xxu@splunk.com>
2016-12-14 11:07:21 +0800

    template transfer

--- package/cloudconnectlib/core/template.py
+++ package/cloudconnectlib/core/template.py
@@ -8,12 +8,11 @@ def compile_template(template):
     _origin_template = template
     _template = Template(template)
 
-    def render_internal(context):
+    def translate_internal(context):
         match = re.match(PATTERN, _origin_template)
         if match:
             context_var = context.get(match.groups()[0])
-            if context_var and isinstance(context_var, (dict, list,tuple)):
-                return context_var
+            return context_var if context_var else ""
         return _template.render(context)
 
-    return render_internal
+    return translate_internal

da20f8604db8a46934860eb6e8c747c3724b70b9
Ke Li <kel@splunk.com>
2016-12-16 11:19:12 +0800

    Remove redundant run()

--- examples/1.0/snow_helsinki/Splunk_TA_snow/bin/snow_inputs.py
+++ examples/1.0/snow_helsinki/Splunk_TA_snow/bin/snow_inputs.py
@@ -2,8 +2,8 @@
 """
 This is the main entry point for My TA
 """
-import re
 import os
+import re
 
 ta_name = os.path.basename(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 ta_lib_name = re.sub("[^\w]+", "_", ta_name.lower())
@@ -11,8 +11,5 @@ __import__(ta_lib_name + "_import_declare")
 
 import cloudconnectlib.splunktacollectorlib.cloud_connect_mod_input as mod_input
 
-def ta_run():
-    mod_input.run()
-
 if __name__ == "__main__":
-    ta_run()
+    mod_input.run()

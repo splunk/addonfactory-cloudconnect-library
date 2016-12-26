a7cdce9aa9e7accb86c360976a93e4bb2f122eec
Ke Li <kel@splunk.com>
2016-12-26 13:39:32 +0800

    Fix addon-12890
    Add a alias method for store value in new variable.
    Make sure all methods return a value.

--- package/cloudconnectlib/core/ext.py
+++ package/cloudconnectlib/core/ext.py
@@ -1,6 +1,9 @@
 import json
 import re
+
+from jsonpath_rw import parse
 from .exceptions import FuncException
+from .pipemgr import PipeManager
 from ..common import util, log
 
 _logger = log.get_cc_logger()
@@ -28,10 +31,10 @@ def regex_not_match(pattern, candidate):
     return not regex_match(pattern, candidate)
 
 
-def json_path(expr, candidate):
+def json_path(json_path_expr, candidate):
     """
     Extract value from string and jsonpath expression with jsonpath.
-    :param expr: jsonpath expression
+    :param json_path_expr: jsonpath expression
     :param candidate: string to extract value
     :return: A `list` contains all values extracted
     """
@@ -40,9 +43,8 @@ def json_path(expr, candidate):
             raise TypeError('candidate expected to be dict or JSON string')
         candidate = json.loads(candidate)
 
-    from jsonpath_rw import parse
-    jsonpath_expr = parse(expr)
-    results = [match.value for match in jsonpath_expr.find(candidate)]
+    expression = parse(json_path_expr)
+    results = [match.value for match in expression.find(candidate)]
     return results[0] if len(results) == 1 else results
 
 
@@ -50,10 +52,23 @@ def splunk_xml(candidates, time=None, index=None, host=None, source=None,
                sourcetype=None):
     """
     Wrap a event with splunk xml format.
+    :param candidates: data used to wrap as event
+    :param time: timestamp which must be empty or a valid float
+    :param index: index name for event
+    :param host: host for event
+    :param source: source for event
+    :param sourcetype: sourcetype for event
     :return: A wrapped event with splunk xml format.
     """
     if not isinstance(candidates, (list, tuple)):
         candidates = [candidates]
+
+    time = time or None
+    if time:
+        try:
+            time = float(time)
+        except ValueError:
+            raise ValueError('"time" must be float: %s' % time)
     return util.format_events(candidates, time=time, index=index, host=host,
                               source=source, sourcetype=sourcetype)
 
@@ -71,10 +86,11 @@ def std_output(candidates):
             all_str = False
             _logger.warning("The type of data needs to print is {} rather "
                             "than basestring".format(type(candidate)))
-        from .pipemgr import PipeManager
         if not PipeManager().write_events(candidate):
             raise FuncException("Fail to output data to stdout. The Event "
                                 "writer has stopped or encountered exception")
+
+    return True
 
 
 def json_empty(json_path_expr, candidate):
@@ -84,6 +100,8 @@ def json_empty(json_path_expr, candidate):
     :param candidate: target to extract
     :return: `True` if the result JSON is `{}` or `[]` or `None`
     """
+    if not candidate:
+        return True
     if json_path_expr:
         candidate = json_path(json_path_expr, candidate)
     if not candidate:
@@ -105,7 +123,14 @@ def json_not_empty(candidate, json_path_expr=None):
     return not json_empty(candidate, json_path_expr)
 
 
+def alias(value):
+    """Alias a variable to another which should be
+    specified in `output`"""
+    return value
+
+
 _EXT_FUNCTIONS = {
+    'alias': alias,
     'regex_match': regex_match,
     'regex_not_match': regex_not_match,
     'splunk_xml': splunk_xml,

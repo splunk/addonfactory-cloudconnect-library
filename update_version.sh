56b50c12c499fbde20e4e262a851aab315f3f270
Eric Guo <eguo@splunk.com>
2017-01-22 09:38:21 +0800

    fix issue in linux platform

--- update_version.sh
+++ update_version.sh
@@ -2,7 +2,6 @@
 
 if [[ $BRANCH == develop ]];
 then
-    version=$(grep __version__ package/cloudconnectlib/__init__.py | awk '{print $3}')
-    version=$(echo $version | sed "s/'//g")
-    sed -i '' "s/$version/$version.dev$BUILDNUMBER/g" package/cloudconnectlib/__init__.py
+    version=$(grep __version__ package/cloudconnectlib/__init__.py | awk '{print $3}' | sed "s/'//g")
+    sed -i'' "s/$version/$version.dev$BUILDNUMBER/g" package/cloudconnectlib/__init__.py
 fi

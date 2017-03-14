990d2bf2c39ca2ea581fd6bf164a2b11bbb6463c
Xiaoqin Chen <xchen@splunk.com>
2017-03-14 16:28:59 +0800

    ADDON-13946: add init logic

--- package/cloudconnectlib/core/plugin.py
+++ package/cloudconnectlib/core/plugin.py
@@ -1,15 +1,88 @@
 from ext import _extension_functions
-import os
+from os import path as op
+from os import walk
+import sys
+from ..common import log
+import traceback
+
+logger = log.get_cc_logger()
 
 
 def cce_pipeline_plugin(func):
-    _extension_functions[func.func_name] = func
+    """
+    Decorator for pipepline plugin functions.
+
+    This docorator helps to register user defined pipeline function into CCE
+    engine so that it could be looked up when executing jobs.
+
+    :param func: User defined function object
+    :type func: ``function``
+
+    Usage::
+        >>> @cce_pipeline_plugin
+        >>> def my_function(arg):
+        >>>     do_work()
+    """
+    if func.func_name in _extension_functions.keys():
+        logger.warning("Pipeline function %s already exists, please rename it!",
+                       func.func_name)
+    else:
+        _extension_functions[func.func_name] = func
+        logger.debug("Added function %s to pipeline plugin system",
+                     func.func_name)
 
     def pipeline_func(*args, **kwargs):
         return func(*args, **kwargs)
     return pipeline_func
 
 
-def init_plugins(plugin_path):
-    if os.path.isdir(plugin_path)
+def import_plugin_file(file_name):
+    """
+    Import a module.
+    1. If the module with the same name already in sys.modules, then log a
+    warning and exit without reloading it.
+    2. If failed to import the file, then log a warning and exit
+    """
+    if file_name.endswith(".py"):
+        module_name = file_name[:-3]
+    else:
+        logger.warning("Plugin file %s is with unsupported extenstion, the "
+                       "supported are py", file_name)
+        return
 
+    if module_name in sys.modules.keys():
+        logger.warning("Module %s aleady exists and it won't be reload, "
+                       "please rename your plugin module if it is required.",
+                       module_name)
+        return
+
+    try:
+        __import__(module_name)
+    except Exception:
+        logger.warning("Failed to load module {}, {}".format(
+            module_name, traceback.format_exc()))
+        return
+
+    logger.info("Module %s is imported", module_name)
+    return
+
+
+def init_pipeline_plugins(plugin_dir):
+    """
+    Initialize the pipeline plugins which triggers the auto registering of user
+    defined pipeline functions.
+    1. Add the plugin_dir into sys.path.
+    2. Import the file under plugin_dir that starts with "cce_plugin_" and ends
+    with ".py"
+    """
+    if not op.isdir(plugin_dir):
+        logger.warning("%s is not a directory! Pipeline plugin files won't be loaded.",
+                       plugin_dir)
+        return
+
+    sys.path.append(plugin_dir)
+    for file_name in next(walk(plugin_dir))[2]:
+        if file_name == "__init__.py" or not file_name.startswith("cce_plugin_"):
+            continue
+        if file_name.endswith(".py"):
+            import_plugin_file(file_name)
--- test/unit/test_plugin.py
+++ test/unit/test_plugin.py
@@ -2,31 +2,182 @@ import pytest
 import sys
 import os
 import common
+import logging
 sys.path.append(os.path.join(common.PROJECT_ROOT, "package"))
+from cloudconnectlib.common import log as ccelog
 
-import cloudconnectlib
-from cloudconnectlib.core.plugin import cce_pipeline_plugin
+logger = logging.getLogger("testing")
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+formatter = logging.Formatter(
+    "%(asctime)s %(levelname)s pid=%(process)d tid=%(threadName)s "
+    "file=%(filename)s:%(funcName)s:%(lineno)d | %(message)s")
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+ccelog.set_cc_logger(logger)
+
+from cloudconnectlib.core.plugin import init_pipeline_plugins
 from cloudconnectlib.core.ext import _extension_functions
-
-
-@cce_pipeline_plugin
-def func_with_arg1(msg):
-    return msg
-
-
-@cce_pipeline_plugin
-def func_with_arg2(msg, post=""):
-    return msg+post
+from cloudconnectlib.core.ext import lookup_method
 
 
 def test_decorator():
+    from cloudconnectlib.core.plugin import cce_pipeline_plugin
+    @cce_pipeline_plugin
+    def func_with_arg1(msg):
+        return msg
+
+    @cce_pipeline_plugin
+    def func_with_arg2(msg, post=""):
+        return msg+post
+
     assert "func_with_arg1" in _extension_functions.keys()
-    ret = func_with_arg1("hello")
+    ret = lookup_method("func_with_arg1")("hello")
     assert ret == "hello"
 
     assert "func_with_arg2" in _extension_functions.keys()
-    ret = func_with_arg2("hello")
+    ret = lookup_method("func_with_arg2")("hello")
     assert ret == "hello"
 
-    ret = func_with_arg2("hello", post="world")
+    ret = lookup_method("func_with_arg2")("hello", post="world")
     assert ret == "hello"+"world"
+
+    _extension_functions.pop("func_with_arg1", None)
+    _extension_functions.pop("func_with_arg2", None)
+
+
+def write_py_file(target_dir, target_file, content):
+    target_file_path = os.path.join(target_dir, target_file)
+    if not os.path.isdir(target_dir):
+        os.mkdir(target_dir)
+
+    with open(target_file_path, mode='w') as tf:
+        tf.write(content)
+
+
+def remove_file(target_dir, target_file):
+    target_file_path = os.path.join(target_dir, target_file)
+    if not os.path.isfile(target_file_path):
+        return
+
+    try:
+        os.remove(target_file_path)
+    except:
+        pass
+
+
+def test_plugin_init_error(capsys):
+    import_part = """
+from cloudconnectlib.core.plugin import cce_pipeline_plugin
+"""
+
+    test_functions = """
+@cce_pipeline_plugin
+def cce_unit_test_func_with_arg1(msg):
+    return msg
+
+@cce_pipeline_plugin
+def cce_unit_test_func_with_arg2(msg, post=""):
+    return msg+post
+
+@cce_pipeline_plugin
+def json_path(msg):
+    return msg
+
+def cce_unit_test_func_without_decorator(msg):
+    return msg
+"""
+    plugin_dir = os.path.join(common.PROJECT_ROOT,
+                              "package",
+                              "cloudconnectlib",
+                              "plugin")
+    test_plugin_file1 = "cce_plugin_test_plugin_file1.py"
+    test_plugin_file2 = "test_plugin_file2.py"
+    test_plugin_file3 = "test_plugin_file3.p"
+    test_plugin_file4 = "plugin.py"
+    test_plugin_file5 = "cce_plugin_test_plugin_file5.py"
+    try:
+        # Don't include the decorator or other import error
+        _extension_functions.pop("cce_unit_test_func_with_arg1", None)
+        _extension_functions.pop("cce_unit_test_func_with_arg2", None)
+        write_py_file(plugin_dir, test_plugin_file1, test_functions)
+        init_pipeline_plugins(plugin_dir)
+        assert "cce_unit_test_func_with_arg1" not in _extension_functions.keys()
+        assert "cce_unit_test_func_with_arg2" not in _extension_functions.keys()
+
+        # Don't with the right prefix
+        write_py_file(plugin_dir, test_plugin_file2, import_part+test_functions)
+        init_pipeline_plugins(plugin_dir)
+        assert "cce_unit_test_func_with_arg1" not in _extension_functions.keys()
+        assert "cce_unit_test_func_with_arg2" not in _extension_functions.keys()
+
+        write_py_file(plugin_dir, test_plugin_file3, import_part+test_functions)
+        init_pipeline_plugins(plugin_dir)
+        assert "cce_unit_test_func_with_arg1" not in _extension_functions.keys()
+        assert "cce_unit_test_func_with_arg2" not in _extension_functions.keys()
+
+        # The file name already exists
+        write_py_file(plugin_dir, test_plugin_file4, import_part+test_functions)
+        init_pipeline_plugins(plugin_dir)
+        assert "cce_unit_test_func_with_arg1" not in _extension_functions.keys()
+        assert "cce_unit_test_func_with_arg2" not in _extension_functions.keys()
+
+        # The function name already exists
+        json_path_value = _extension_functions["json_path"]
+        write_py_file(plugin_dir, test_plugin_file5, import_part+test_functions)
+        init_pipeline_plugins(plugin_dir)
+        assert "cce_unit_test_func_with_arg1" in _extension_functions.keys()
+        assert "cce_unit_test_func_with_arg2" in _extension_functions.keys()
+        assert _extension_functions["json_path"] == json_path_value
+
+    finally:
+        remove_file(plugin_dir, test_plugin_file1)
+        remove_file(plugin_dir, test_plugin_file1+"c")
+        remove_file(plugin_dir, test_plugin_file2)
+        remove_file(plugin_dir, test_plugin_file2+"c")
+        remove_file(plugin_dir, test_plugin_file3)
+        remove_file(plugin_dir, test_plugin_file4)
+        remove_file(plugin_dir, test_plugin_file4+"c")
+        remove_file(plugin_dir, test_plugin_file5)
+        remove_file(plugin_dir, test_plugin_file5+"c")
+
+
+def test_plugin_init(capsys):
+
+    test_functions = """
+from cloudconnectlib.core.plugin import cce_pipeline_plugin
+
+@cce_pipeline_plugin
+def cce_unit_test_func_with_arg1(msg):
+    return msg
+
+@cce_pipeline_plugin
+def cce_unit_test_func_with_arg2(msg, post=""):
+    return msg+post
+
+def cce_unit_test_func_without_decorator(msg):
+    return msg
+"""
+    plugin_dir = os.path.join(common.PROJECT_ROOT,
+                              "package",
+                              "cloudconnectlib",
+                              "plugin")
+    test_plugin_file = "cce_plugin_test_plugin_file.py"
+    try:
+        _extension_functions.pop("cce_unit_test_func_with_arg1", None)
+        _extension_functions.pop("cce_unit_test_func_with_arg2", None)
+        write_py_file(plugin_dir, test_plugin_file, test_functions)
+        init_pipeline_plugins(plugin_dir)
+
+        assert "cce_unit_test_func_with_arg1" in _extension_functions.keys()
+        ret = lookup_method("cce_unit_test_func_with_arg1")("hello")
+        assert ret == "hello"
+
+        assert "cce_unit_test_func_with_arg2" in _extension_functions.keys()
+        ret = lookup_method("cce_unit_test_func_with_arg2")("hello", "you")
+        assert ret == "hello"+"you"
+
+        assert "cce_unit_test_func_without_decorator" not in _extension_functions.keys()
+    finally:
+        remove_file(plugin_dir, test_plugin_file)
+        remove_file(plugin_dir, test_plugin_file+"c")

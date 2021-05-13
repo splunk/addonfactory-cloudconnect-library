149146cb5da4d7ae5a5e2fbc7e1a74ac194178a1
Addon Factory template <addonfactory@splunk.com>
2021-05-13 12:33:50 -0400

    chore: Cleanup and convert to poetry

--- update_version.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [[ $BRANCH == develop ]];
-then
-    version=$(grep __version__ package/cloudconnectlib/__init__.py | awk '{print $3}' | sed "s/'//g")
-    sed -i'' "s/$version/$version.dev$BUILDNUMBER/g" package/cloudconnectlib/__init__.py
-fi
